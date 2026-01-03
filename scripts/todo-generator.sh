43f09146311481a82829c1f0142a267f125d6dfe
Daniel Silva <silva20102@gmail.com>
2026-01-03 05:45:13 -0300

    Update todo-generator.sh

--- scripts/todo-generator.sh
+++ scripts/todo-generator.sh
@@ -3,51 +3,55 @@
 OUTPUT_DIR="melhorias"
 mkdir -p "$OUTPUT_DIR"
 
-# Limpa arquivos antigos
-# rm -f "$OUTPUT_DIR"/*.md
-
 grep -Rn "//TODO: \[" ./ --include="*.java" \
 | while IFS=: read -r file lineNumber line; do
     class=$(basename "$file" .java)
 
-    if [ -f "$OUTPUT_DIR/$class.md" ]; then
-      continue
-    fi
-
-    # Extrai categoria entre []
     category=$(echo "$line" | sed -n 's/.*TODO: \[\(.*\)\] -.*/\1/p')
-
-    # Extrai texto após - []
     todo=$(echo "$line" | sed 's/.*- \[\] /- [ ] /')
 
     echo "$class|$category|$todo ($lineNumber)"
 done \
 | sort \
 | awk -F'|' -v out="$OUTPUT_DIR" '
- {
-   file = out "/" $1 ".md"
-   class = $1
-   category = $2
-   item = $3
+{
+  file = out "/" $1 ".md"
+  class = $1
+  category = $2
+  item = $3
 
-   # Cabeçalho do arquivo (uma vez por classe)
-   if (!printedFile[file]++) {
-     print "## Débitos Técnicos" > file
-     print "" >> file
-     print "### " class >> file
-   }
+  # Cabeçalho do arquivo
+  if (!printedFile[file]++) {
+    print "## Débitos Técnicos" > file
+    print "" >> file
+    print "### " class >> file
+  }
 
-   # Cabeçalho da categoria (uma vez por classe + categoria)
-   key = file "|" category
-   if (!printedCategory[key]++) {
-     print "" >> file
-     print "#### " category >> file
-   }
+  # Detecta troca de categoria (ou primeira)
+  if (currentCategory != "" && (category != currentCategory || file != currentFile)) {
+    print "- [Regra]:" >> currentFile
+  }
 
-   # Item (SEM linha em branco extra)
-   print item >> file
-   print "    - [Regra]:" >> file
- }
- '
+  # Cabeçalho da nova categoria
+  key = file "|" category
+  if (!printedCategory[key]++) {
+    print "" >> file
+    print "#### " category >> file
+  }
+
+  # Item
+  print item >> file
+
+  currentCategory = category
+  currentFile = file
+}
+
+END {
+  # Fecha última categoria
+  if (currentCategory != "") {
+    print "- [Regra]:" >> currentFile
+  }
+}
+'
 
 echo "✅ Arquivos de melhorias gerados em ./$OUTPUT_DIR/"
